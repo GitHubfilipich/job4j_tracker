16a2bf97940b5cfb2f74c40b74a5c4bc90ee6b73
YuryGe <postbox177@yandex.ru>
2023-04-27 22:36:22 +0400

    0. Map, HashMap [#212652] - исправление

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("postbox177@yandex.ru", "Pushkin Aleksandr Sergeevich");
+        map.put("mail@yandex.ru", "Mailov Yandex Ruevich");
+        map.put("yandex@mail.ru", "Yandexov Mail Ruevich");
+        map.put("postbox177@yandex.ru", "Aleksandrov Sergey Pushkinovich");
         for (String key : map.keySet()) {
             System.out.println(key + ": " + map.get(key));
         }
