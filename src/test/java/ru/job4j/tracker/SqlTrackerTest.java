b85226071f2ef35a2e72a9404e848fda06df854e
YuryGe <postbox177@yandex.ru>
2024-03-14 10:44:10 +0400

    3. Liquibase. Интеграционные тесты. [#504894]

---
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -0,0 +1,112 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SqlTrackerTest {
+
+    private static Connection connection;
+
+    @BeforeAll
+    public static void initConnection() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("db/liquibase_test.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @AfterAll
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    @AfterEach
+    public void wipeTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("delete from items")) {
+            statement.execute();
+        }
+    }
+
+    @Test
+    public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertThat(tracker.findById(item.getId())).isEqualTo(item);
+    }
+
+    @Test
+    public void whenReplaceItemThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        Item newItem = new Item(item.getId(), "New item");
+        tracker.replace(item.getId(), newItem);
+        assertThat(tracker.findById(newItem.getId())).isEqualTo(newItem);
+    }
+
+    @Test
+    public void whenDeleteItemThenMustBeAbsent() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        tracker.delete(item.getId());
+        assertThat(tracker.findById(item.getId())).isNull();
+    }
+
+    @Test
+    public void whenFindAllItemsThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        Item item2 = new Item("item 2");
+        tracker.add(item2);
+        Item item3 = new Item("item 3");
+        tracker.add(item3);
+        assertThat(tracker.findAll()).containsExactly(item, item2, item3);
+    }
+
+    @Test
+    public void whenFindByNameItemsThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        Item item2 = new Item("idem 2");
+        tracker.add(item2);
+        Item item3 = new Item("item 3");
+        tracker.add(item3);
+        assertThat(tracker.findByName("tem")).containsExactly(item, item3);
+    }
+
+    @Test
+    public void whenFindByIdItemsThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        Item item2 = new Item("idem 2");
+        tracker.add(item2);
+        Item item3 = new Item("item 3");
+        tracker.add(item3);
+        assertThat(tracker.findById(item2.getId())).isEqualTo(item2);
+    }
+}
