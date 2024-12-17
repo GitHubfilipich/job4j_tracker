93079b77998ac08ddeba90d9a5cda566ec6a59e1
YuryGe <postbox177@yandex.ru>
2024-12-17 15:28:37 +0400

    1. Что такое Mock-объект - исправление

--- src/test/java/ru/job4j/tracker/FindByNameActionTest.java
+++ src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -2,8 +2,6 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -15,8 +13,8 @@ class FindByNameActionTest {
     public void whenItemWasFoundSuccessfully() {
         Output output = new StubOutput();
         MemTracker tracker = new MemTracker();
-        tracker.add(new Item(1, "New item",
-                LocalDateTime.of(2024, 12, 17, 0, 0, 0)));
+        Item item = new Item("New item");
+        tracker.add(item);
         FindByNameAction action = new FindByNameAction(output);
 
         Input input = mock(Input.class);
@@ -28,7 +26,7 @@ class FindByNameActionTest {
         String ln = System.lineSeparator();
         assertThat(output.toString()).isEqualTo(
                 "=== Find items by name ===" + ln
-                        + "Item{id=1, name='New item', created=17-декабря-вторник-2024 00:00:00}" + ln
+                        + item + ln
         );
     }
 
